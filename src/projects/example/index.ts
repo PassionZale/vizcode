58a3a7d63839c39c817ef8c46710a32fbfa25baf
Lei Zhang <372470745@qq.com>
2025-08-29 14:34:21 +0800

    Add new funny scene with binary search animation and update project settings

--- src/projects/example/index.ts
+++ src/projects/example/index.ts
@@ -2,7 +2,8 @@ import { makeProject } from "@motion-canvas/core";
 
 import example from "./scenes/example?scene";
 import code from "./scenes/code?scene";
+import funny from "./scenes/funny?scene";
 
 export default makeProject({
-  scenes: [example, code],
+  scenes: [funny, example, code],
 });

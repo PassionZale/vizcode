57a0f49a5ceaf3df32b86e093731670e2d5bb6d6
Lei Zhang <372470745@qq.com>
2025-10-22 18:35:04 +0800

    feat(code-journey): 更新 2016_2018 场景为 CodeSugar 品牌展示
    将场景内的占位文本替换为 "CodeSugar"，其中 "Code" 部分采用四色设计以增强品牌感。为 logo 添加了黄色背景以提升视觉效果。同时移除了未使用的导入并统一了代码格式。

--- src/projects/code-journey/scenes/2016_2018.tsx
+++ src/projects/code-journey/scenes/2016_2018.tsx
@@ -1,35 +1,55 @@
-import { Img, Layout, makeScene2D, Rect, Txt } from "@motion-canvas/2d";
+import { Img, makeScene2D, Rect, Txt } from "@motion-canvas/2d";
 import { waitFor } from "@motion-canvas/core";
 import logoSvg from "../assets/logo.svg";
 import { TextStyles } from "../../../shared/text-styles";
-
-// #101014
-// #121b21
-// #ffffff1a
 
 export default makeScene2D(function* (view) {
   view.add(
     <Rect layout size={["100%", "100%"]} fill={"#121b21"} direction={"column"}>
       <Rect
-        size={['100%', 400]}
+        size={["100%", '25%']}
         layout
-				padding={[40, 20]}
+        padding={[40, 20]}
         direction="column"
         alignItems="center"
         justifyContent="space-between"
-				gap={40}
+        gap={40}
       >
-        <Img size={[160, 160]} src={logoSvg} fill={"yellow"}/>
+        <Rect padding={20} fill={"#ffcc00"}>
+          <Img size={[160, 160]} src={logoSvg} />
+        </Rect>
 
-        <Txt {...TextStyles.title}>Title</Txt>
+        <Txt {...TextStyles.title}>Code Journey</Txt>
       </Rect>
 
       <Rect grow={1}>
         <Txt {...TextStyles.subtitle}>456</Txt>
       </Rect>
 
-      <Rect size={["100%", 400]}>
-        <Txt {...TextStyles.subtitle}>789</Txt>
+      <Rect
+        size={["100%", '25%']}
+        layout
+        padding={40}
+        alignItems={"start"}
+        justifyContent={"center"}
+      >
+        <Txt {...TextStyles.title} fill={"#4285F4"}>
+          C
+        </Txt>
+        <Txt {...TextStyles.title} fill={"#FBBC05"}>
+          o
+        </Txt>
+        <Txt {...TextStyles.title} fill={"#EA4335"}>
+          d
+        </Txt>
+        <Txt {...TextStyles.title} fill={"#34A853"}>
+          e
+        </Txt>
+        <Txt {...TextStyles.title}>S</Txt>
+        <Txt {...TextStyles.title}>u</Txt>
+        <Txt {...TextStyles.title}>g</Txt>
+        <Txt {...TextStyles.title}>a</Txt>
+        <Txt {...TextStyles.title}>r</Txt>
       </Rect>
     </Rect>
   );

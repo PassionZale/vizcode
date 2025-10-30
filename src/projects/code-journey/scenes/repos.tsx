e6aa927b7d34952c95ddd912e588c098e0a50230
Lei Zhang <372470745@qq.com>
2025-10-30 15:46:05 +0800

    chore: 删除 code-journey 项目中的冗余资源文件
    删除了 src/projects/code-journey/assets/repos 目录下的多个不再使用的图像文件，包括 blog.png、create-app.png、geist-design.svg、release-viewer.svg 和 tsch.svg。这些文件的移除有助于减少项目体积并保持资源目录的整洁。

--- src/projects/code-journey/scenes/repos.tsx
+++ src/projects/code-journey/scenes/repos.tsx
@@ -1,35 +1,49 @@
-import { Icon, Img, Latex, makeScene2D, Rect, Txt } from "@motion-canvas/2d";
-import { all, createRef, waitUntil } from "@motion-canvas/core";
+import {
+  Icon,
+  Img,
+  makeScene2D,
+  Rect,
+  Txt,
+  Latex,
+  RectProps,
+} from "@motion-canvas/2d";
+import { all, createRef, sequence, waitUntil } from "@motion-canvas/core";
 import { TextStyles } from "@/shared/text-styles";
 import logoSvg from "../assets/logo.svg";
-import blogPng from "../assets/repos/blog.png";
-import createAppPng from "../assets/repos/create-app.png";
-import geistDesingSvg from "../assets/repos/geist-design.svg";
-import releaseViewerSvg from "../assets/repos/release-viewer.svg";
-import tschSvg from "../assets/repos/tsch.svg";
+import slidevSvg from "../assets/slidev.svg";
 import { appear } from "@/shared/utils";
 
 const REPOS = [
   {
-    title: "我的博客，基于 `Astro`",
-    icon: blogPng,
+    title: "Type Challenges",
+    icon: "skill-icons:typescript",
+    desc: "我的 TS 类型体操。",
   },
   {
-    title: "我的 TS 类型体操",
-    icon: tschSvg,
+    title: "Create App",
+    icon: "skill-icons:npm-light",
+    desc: "我的脚手架，基于 `Nodejs`。",
   },
   {
-    title: "我的组件库，基于 `Vuejs`",
-    icon: geistDesingSvg,
+    title: "lovchun.com",
+    icon: "skill-icons:astro",
+    desc: "我的博客，基于 `Astro`。",
   },
-  // {
-  //   title: "我的脚手架，基于 `Nodejs`",
-  //   icon: createAppPng,
-  // },
-  // {
-  //   title: "我的发布器，基于 `Nextjs`",
-  //   icon: releaseViewerSvg,
-  // },
+  {
+    title: "Release Viewer",
+    icon: "skill-icons:nextjs-light",
+    desc: "我的发布器，基于 `Nextjs`。",
+  },
+  {
+    title: "Geist Design",
+    icon: "skill-icons:vuejs-light",
+    desc: "我的组件库，基于 `Vuejs`。",
+  },
+  {
+    title: "Talks",
+    img: slidevSvg,
+    desc: "我的演讲，基于 `Slidev`。",
+  },
 ];
 
 export default makeScene2D(function* (view) {
@@ -38,6 +52,11 @@ export default makeScene2D(function* (view) {
   const rectRefs = Array.from({ length: REPOS.length }, () =>
     createRef<Rect>()
   );
+  const repoRefs = Array.from({ length: REPOS.length }, () =>
+    createRef<Rect>()
+  );
+
+  const rectSize: RectProps["size"] = [(view.width() - 90) / 2, 200];
 
   yield view.add(
     <Rect layout size={["100%", "100%"]} fill={"#121b21"} direction={"column"}>
@@ -60,7 +79,7 @@ export default makeScene2D(function* (view) {
           alignItems={"center"}
           justifyContent={"center"}
         >
-          <Icon icon="tabler:brand-github" size={56} />
+          <Icon icon="simple-icons:github" size={56} />
           <Txt {...TextStyles.body}>@</Txt>
           <Txt {...TextStyles.subtitle} fontWeight={500}>
             PassionZale
@@ -69,22 +88,7 @@ export default makeScene2D(function* (view) {
       </Rect>
 
       <Rect grow={1}>
-        <Rect ref={contentRef} layout={false}>
-          {/* <Rect width={'30%'} height={800} fill="#fff" layout>
-            <Img src={blogPng} size={100} radius={60} />
-
-            <Rect layout direction={"column"}>
-              <Txt {...TextStyles.subtitle}>lovchun.com-next</Txt>
-              <Txt {...TextStyles.caption}>
-                articles and happiness I want share.
-              </Txt>
-            </Rect>
-          </Rect> */}
-          {/* <Rect size={300} fill={"#FBBC05"}></Rect>
-          <Rect size={300} fill={"#EA4335"}></Rect>
-          <Rect size={300} fill={"#ffcc00"}></Rect>
-          <Rect size={300} fill={"#34A853"}></Rect> */}
-        </Rect>
+        <Rect ref={contentRef} layout={false} />
       </Rect>
 
       <Rect
@@ -117,16 +121,68 @@ export default makeScene2D(function* (view) {
 
   rectRefs.forEach((ref) => {
     contentRef().add(
-      <Rect ref={ref} size={300} stroke={"white"} lineWidth={5} />
+      <Rect
+        ref={ref}
+        size={rectSize}
+        stroke={"#666"}
+        lineWidth={2}
+        radius={20}
+      />
     );
   });
 
   yield* all(...rectRefs.map((ref) => appear(ref())));
 
   yield* all(
-    rectRefs[0]().right(rectRefs[1]().left().addX(-50), 1),
-    rectRefs[2]().left(rectRefs[1]().right().addX(50), 1)
+    rectRefs[0]().top(rectRefs[1]().bottom().addY(40), 1),
+    rectRefs[3]().top(rectRefs[4]().bottom().addY(40), 1),
+    rectRefs[2]().bottom(rectRefs[1]().top().addY(-40), 1),
+    rectRefs[5]().bottom(rectRefs[4]().top().addY(-40), 1)
   );
+
+  yield* all(
+    rectRefs[0]().x(-265, 1),
+    rectRefs[1]().x(-265, 1),
+    rectRefs[2]().x(-265, 1),
+    rectRefs[3]().x(265, 1),
+    rectRefs[4]().x(265, 1),
+    rectRefs[5]().x(265, 1)
+  );
+
+  yield repoRefs.forEach((ref, i) => {
+    contentRef().add(
+      <Rect
+        ref={ref}
+        opacity={0}
+        layout
+        size={rectSize}
+        direction={"column"}
+        paddingLeft={30}
+        alignItems={"start"}
+        justifyContent={"space-around"}
+      >
+        <Rect layout gap={20} direction={"row"} alignItems={"center"}>
+          {/* <Icon icon="iconoir:repository" size={60} /> */}
+          {REPOS[i].icon ? (
+            <Icon icon={REPOS[i].icon} size={60} />
+          ) : (
+            <Img src={REPOS[i].img} size={60} />
+          )}
+          <Latex tex={REPOS[i].title} fontSize={40} fill={"#fff"} />
+        </Rect>
+
+        <Txt fontSize={32} fill={"#ffffffcc"}>
+          {REPOS[i].desc}
+        </Txt>
+      </Rect>
+    );
+  });
+
+  repoRefs.forEach((ref, i) => {
+    ref().position(rectRefs[i]().position());
+  });
+
+  yield* sequence(0.15, ...repoRefs.map((ref) => appear(ref())));
 
   yield* waitUntil("repo_list_end");
 });

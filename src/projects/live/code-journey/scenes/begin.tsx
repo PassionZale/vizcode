9283ffabb6d77753654f408c9be177bc2cf58dd2
Lei Zhang <372470745@qq.com>
2025-11-09 15:08:39 +0800

    feat(scenes): 添加代码之旅叙事文本和动画
    - 添加了四个文本元素展示编程故事
    - 使用chain动画序列展示中文文本
    - 优化了场景布局和动画时序

--- src/projects/live/code-journey/scenes/begin.tsx
+++ src/projects/live/code-journey/scenes/begin.tsx
@@ -1,11 +1,14 @@
 import { Img, makeScene2D, Rect, Txt } from "@motion-canvas/2d";
-import { createRef } from "@motion-canvas/core";
+import { chain, createRef, sequence } from "@motion-canvas/core";
 import logoSvg from "../assets/logo.svg";
 import { TextStyles } from "@/shared/text-styles";
 import { waitForCut } from "@/shared/utils";
 
 export default makeScene2D(function* (view) {
   const titleRef = createRef<Txt>();
+  const contentRef = createRef<Rect>();
+  const txtRef = createRef<Txt>();
+  const txtRefs = Array.from({ length: 4 }, () => createRef<Txt>());
 
   yield view.add(
     <Rect layout size={["100%", "100%"]} fill={"#121b21"} direction={"column"}>
@@ -24,7 +27,20 @@ export default makeScene2D(function* (view) {
         <Txt ref={titleRef} {...TextStyles.title}></Txt>
       </Rect>
 
-      <Rect grow={1}></Rect>
+      <Rect
+        grow={1}
+        ref={contentRef}
+        layout
+        direction={"column"}
+        paddingLeft={200}
+        gap={50}
+        alignItems={"start"}
+        justifyContent={"center"}
+      >
+        {txtRefs.map((ref) => (
+          <Txt ref={ref} {...TextStyles.subtitle} minHeight={65} minWidth={60}></Txt>
+        ))}
+      </Rect>
 
       <Rect
         size={["100%", "25%"]}
@@ -54,7 +70,14 @@ export default makeScene2D(function* (view) {
     </Rect>
   );
 
-  yield* titleRef().text("Code Journey", 1);
+  yield* titleRef().text("Code Journey", 0.5);
+
+  yield* chain(
+    txtRefs[0]().text("十年前，", 0.5),
+    txtRefs[1]().text("我写下了人生中的第一行代码，", 1),
+    txtRefs[2]().text("我的代码没有改变这个世界，", 1),
+    txtRefs[3]().text("但是编程却改变了我的生活。", 1)
+  );
 
   yield* waitForCut();
 });
